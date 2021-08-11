f854ebfda1074b1df2bdfdc3db330b06486962ff
Ronca Damiano <d.ronca@reply.it>
2021-08-11 11:31:11 +0200

    scaffolding lato server identity core

---
+++ Fondital.Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Fondital.Data;
+using Fondital.Shared.Models.Auth;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Fondital.Server.Areas.Identity.IdentityHostingStartup))]
+namespace Fondital.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
