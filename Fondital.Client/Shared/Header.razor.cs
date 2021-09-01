1baccff1b8c59d113aa56ddf6d31a739e21f5952
Elia Di Profio <elia.diprofio@aesys.tech>
2021-09-01 10:38:30 +0200

    Correzione Header.razor.cs

--- Fondital.Client/Shared/Header.razor.cs
+++ Fondital.Client/Shared/Header.razor.cs
@@ -7,10 +7,6 @@ namespace Fondital.Client.Shared
 {
     public partial class Header
     {
-        string UserFirstName = string.Empty;
-        string UserLastName = string.Empty;
-        string CurrentLanguage = string.Empty;
-        List<string> Languages;
         string CurrentAnag = string.Empty;
         List<string> Anags;
         List<string> UserMenuList;
@@ -18,26 +14,18 @@ namespace Fondital.Client.Shared
 
         protected override Task OnInitializedAsync()
         {
-            Languages = new List<string>
-            {
-                "ITA", "RUS"
-            };
-
-            CurrentLanguage = "ITA";
             Anags = new List<string>
-                {
-                    "Anagrafiche SP", "Anagrafiche Difetti", "Anagrafiche Lavorazioni", "Anagrafiche Voci di Costo", "Listino"
-                };
+            {
+                localizer["AnagraficheSP"], localizer["AnagraficheDifetti"], localizer["AnagraficheLavorazioni"], localizer["AnagraficheVociDiCosto"], localizer["Listino"]
+            };
 
             CurrentAnag = null;
             UserMenuList = new List<string>
-                    {
-                        "Cambia Password", "LogOut"
-                        };
+            {
+                localizer["CambiaPassword"], localizer["Esci"]
+            };
 
             ViewUserMenu = false;
-            UserFirstName = "Lupo";
-            UserLastName = "Lucio";
 
             return base.OnInitializedAsync();
         }
