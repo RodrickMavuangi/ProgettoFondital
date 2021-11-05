17cc384292e3ec706e38796dec5eaea05dc542c9
Ronca Damiano <d.ronca@reply.it>
2021-11-05 09:23:23 +0100

    hotfix selezione menu header

--- Fondital.Client/wwwroot/js/menu.js
+++ Fondital.Client/wwwroot/js/menu.js
@@ -1,32 +1,22 @@
-﻿let actualUri = '/';
+let actualUri = '/';
 
-function manipulateActiveLink(firstRender, uri)
-{
-    console.log(uri);
+function manipulateActiveLink(firstRender, uri) {
+
     if (!firstRender)
         return;
 
     actualUri = '/' + uri;
     let activeAnchor = document.querySelector("[href='" + actualUri + "']");
-    if (activeAnchor === null)
-    {
+    if (activeAnchor === null) {
         activeAnchor = document.querySelector('a.active');
         //activeAnchor = document.querySelector("span.k-menu-link > a.active");
     }
 
     activeAnchor.parentElement.classList.add('bg_active');
-    
 
     let anagraficaLiElement = document.querySelector("[aria-haspopup=true]");
-    
 
-
-    anagraficaLiElement.addEventListener('mouseenter', e => {
-        OnClickHandler({ url: actualUri });
-    });
-
-    anagraficaLiElement.addEventListener('mouseleave', e => {
-        
+    anagraficaLiElement.addEventListener('load', e => {
         OnClickHandler({ url: actualUri });
     });
 }
@@ -39,15 +29,20 @@ function OnClickHandler(item) {
         span.classList.remove('bg_active');
     });
 
+    var linkAttivo = document.querySelector('.active');
+    var nodoSpan = linkAttivo.closest(".k-menu-link");
+    var nodoLI = linkAttivo.closest(".k-menu-item");
+    
     if (item !== null) {
         let activeAnchor = document.querySelector("[href='" + item.url + "']");
-
         activeAnchor.parentElement.classList.add("bg_active");
 
         if (activeAnchor.parentElement.parentElement.parentElement.parentElement.classList.contains('k-menu-popup')) {
-            document.querySelector("[aria-haspopup=true]").firstChild.classList.add("bg_active");
+            document.querySelector("[aria-haspopup=true]").firstChild.classList.add("bg_active_padre");
+            nodoLI.classList.add("bg_active");
             document.querySelector("[aria-haspopup=true]").setAttribute('aria-hasopenmenu', true);
         } else if (!item.ModifyOnClick) {
+            nodoLI.classList.remove("bg_active");
         } else {
             document.querySelector("[aria-hasopenmenu=true]").setAttribute('aria-hasopenmenu', false);
         }
