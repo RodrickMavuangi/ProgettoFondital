304d08200863f0b043d7de67ed10ae3d8ef21663
Elia Di Profio <elia.diprofio@aesys.tech>
2021-11-30 10:21:38 +0100

    Chiamata Ricambi

--- Fondital.Shared/Dto/RicambioRequestDto.cs
+++ Fondital.Shared/Dto/RicambioRequestDto.cs
@@ -12,8 +12,8 @@ namespace Fondital.Shared.Dto
 
         [Display(Name = "IdFornitore", ResourceType = typeof(Resources.Display))]
         [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources.Validation))]
-        [Range(0, Int32.MaxValue, ErrorMessageResourceName = "OutOfBounds", ErrorMessageResourceType = typeof(Resources.Validation))]
-        public int? SupplierId { get; set; }
+        //[Range(0, Int32.MaxValue, ErrorMessageResourceName = "OutOfBounds", ErrorMessageResourceType = typeof(Resources.Validation))]
+        public string? SupplierId { get; set; }
 
         [Display(Name = "Quantita", ResourceType = typeof(Resources.Display))]
         [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(Resources.Validation))]
